936ae2da6ec06ed2e4514848f95c818fab5e1674
Jeevan s <sathishjeevan2004@gmail.com>
2026-05-30 23:17:33 +0530

    redux store was created with slice as counterSlice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { Provider } from "react-redux";
+import counterStore from "./reduxState/store/counterStore.js";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={counterStore}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
